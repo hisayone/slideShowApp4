739169b778d28a06ff9d6fa381e8eacf68a2362d
hi <hisa.yone@hotmail.com>
2020-05-07 00:33:40 +0900

    Initial Commit

---
+++ slideShowApp4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideShowApp4
+//
+//  Created by 米島尚 on 2020/05/07.
+//  Copyright © 2020 hisa.yone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
